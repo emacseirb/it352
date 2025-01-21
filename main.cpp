025f0c7f82ffcb7edd296d204c7e840151346731
PROUTEAU Guillaume <gprouteau@djembe.pedago.ipb.fr>
2025-01-21 14:25:12 +0100

    ticker_without_whiletrue

--- main.cpp
+++ main.cpp
@@ -5,38 +5,24 @@
 
 #include "mbed.h"
 
-using namespace std::chrono;
+Ticker flipper;
 
-InterruptIn button(BUTTON1);
 DigitalOut led(LED1);
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-long long unsigned int counter = 0;
-
-Timer t;
 
 void flip(){
-    t.start();
-    led = 1;
-}
-
-void flip_2(){
-    t.stop();
-    counter = duration_cast<milliseconds>(t.elapsed_time()).count();
-    led = 0;
-    t.reset();
+    led = !led;
 }
 
 int main()
 {
     printf("start succesfully !!\n");
-    button.rise(&flip);
-    button.fall(&flip_2);
+    flipper.attach(&flip, 0.5);
     while(1){
-        printf("The duration of the button push was : %llu\n", counter);
-        ThisThread::sleep_for(500);
+        ThisThread::sleep_for(200);
         
     }
 
