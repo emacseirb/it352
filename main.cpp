2dadd89c0615957e284aee514d5f5b470e76f1ea
PROUTEAU Guillaume <gprouteau@djembe.pedago.ipb.fr>
2025-01-21 16:19:32 +0100

    communication avec le capteur

--- main.cpp
+++ main.cpp
@@ -22,44 +22,57 @@ UART donc pas d'adresse de registre
     - send : 0x68 - 0x01 - 0x02 - data sur 1 octet
     - response (ACK) : 0xA5A5
 */
-
+#include "hpma115/hpma115.h"
 #include "mbed.h"
 
-Ticker flipper;
-InterruptIn button(BUTTON1);
-DigitalOut led(LED1);
 
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
-int flag = 0;
-int mode = 100000;
-
-void speed(){
-    flag = 1;
+namespace {
+#define WAIT 1s
 }
-
-void flip(){
-    led = !led;
-}
-
-
+using namespace sixtron;
+static DigitalOut led(LED1);
+static hpma115_data_t data;
 
 int main()
 {
-    printf("start succesfully !!\n");
-    button.rise(&speed);
+    uint8_t coef;
+    HPMA115::ErrorType err;
 
-    while(1){
-        if (flag == 1){
-            mode += 100000;
-            flag = 0;
-            flipper.attach_us(&flip, mode);
-            printf("mode : %d us\n", (int)mode);
+    HPMA115 sensor(P1_UART_TX, P1_UART_RX);
+
+    printf("\n\n------------\nHPMA115 Example\n");
+
+    err = sensor.stop_measurement();
+    assert(err == HPMA115::ErrorType::Ok);
+
+    err = sensor.stop_autosend();
+    assert(err == HPMA115::ErrorType::Ok);
+
+    err = sensor.set_adjust_coef(200);
+    assert(err == HPMA115::ErrorType::Ok);
+
+    err = sensor.read_adjust_coef(&coef);
+    assert(err == HPMA115::ErrorType::Ok);
+    assert(coef == 200);
+
+    err = sensor.set_adjust_coef(100);
+    assert(err == HPMA115::ErrorType::Ok);
+
+    err = sensor.start_measurement();
+    assert(err == HPMA115::ErrorType::Ok);
+
+    while (true) {
+        led = !led;
+        err = sensor.read_measurement(&data);
+        if (err == HPMA115::ErrorType::Ok) {
+            printf("Data: ");
+            if (data.pm1_pm4_valid) {
+                printf("PM1.0: %d, PM4.0: %d ", data.pm1_0, data.pm4_0);
+            }
+            printf("PM10: %d, PM2.5: %d\n", data.pm10, data.pm2_5);
         }
-        ThisThread::sleep_for(200);
-        
+
+        ThisThread::sleep_for(WAIT);
     }
 
 }
